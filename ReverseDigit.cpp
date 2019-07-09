8b72eb64bbd4f5627c0ff42148daa7e783c307e5
Jack Ross <43938405+JackRossProjects@users.noreply.github.com>
2019-07-09 08:04:24 -0600

    Add files via upload

---
+++ ReverseDigit.cpp
@@ -0,0 +1,30 @@
+// Jack Ross
+// Reverse Digit
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int userNumber;
+	int reversedNumber = 0;
+	int modLeftover;
+
+	cout << "Enter a number and watch it be magically reversed!" << endl;
+	cout << "Your number is: ";
+
+	cin >> userNumber;
+
+	while (userNumber != 0) {
+
+		modLeftover = userNumber % 10;
+		reversedNumber = reversedNumber * 10 + modLeftover;
+		userNumber = userNumber / 10;
+
+	}
+
+	cout << "Your number REVERSED is: ";
+	cout << reversedNumber << endl;
+	
+}
+
+
